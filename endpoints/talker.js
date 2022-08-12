e3e79798336afba5bcf5d73833773e9a81e854cd
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-08-12 19:42:41 -0300

    requisito 2 ok

--- endpoints/talker.js
+++ endpoints/talker.js
@@ -4,6 +4,7 @@ const { readFs } = require('../middlewares/readWrite');
 // const talkerJson = require('../talker.json');
 
 const OK_STATUS = 200;
+const NOT_OK_STATUS = 404;
 
 const talker = express.Router();
 
@@ -13,6 +14,20 @@ talker.get('/talker', rescue(async (req, res) => {
     const empty = [];
     if (result === empty) return res.status(OK_STATUS).json(empty);
     return res.status(OK_STATUS).json(result);
+  } catch (error) {
+    return (error);
+  }
+}));
+
+talker.get('/talker/:id', rescue(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await readFs();
+    const resultId = result.find((person) => person.id === +id);
+    if (!resultId) {
+      return res.status(NOT_OK_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(OK_STATUS).json(resultId);
   } catch (error) {
     return (error);
   }
